d9621fcb87a5dfa3c4e1a2de20a5d5f646d464e7
Jose Medina <josem419@stanford.edu>
2026-03-08 22:44:08 -0700

    specification submodules

---
+++ src/verification/specifications/aircraft_heading_spec.py
@@ -0,0 +1,6 @@
+""" Library with specification defintiions for the aircraaft heading guidance generation system"""
+
+
+
+# instead of a very generalized specifiction modeling langauge
+# we define discrete functions that take in a trajectory, and possibly a true trajectory, and return booleans
